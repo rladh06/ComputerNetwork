c297ec32b1164f3f69b25020d298a1c764d8e1ca
CSJ <98388286+CanSJun@users.noreply.github.com>
2022-10-06 17:25:23 +0900

    Update

--- ARP/src/EthernetLayer.java
+++ ARP/src/EthernetLayer.java
@@ -1,4 +1,3 @@
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutput;
@@ -36,6 +35,7 @@ public class EthernetLayer implements BaseLayer {
 			this.enet_dstaddr = new _ETHERNET_ADDR();
 			this.enet_srcaddr = new _ETHERNET_ADDR();
 			this.enet_type = new byte[2];
+			this.enet_type[0] = (byte) 0x08; // 0x0800
 			this.enet_data = null;
 		}
 	}
@@ -48,29 +48,80 @@ public class EthernetLayer implements BaseLayer {
 		pLayerName = pName;
 		
 	}
+	public byte[] ObjToByte(_ETHERNET_HEADER Header, byte[] input, int length) {
+		byte[] buf = new byte[length + 14];
 
+		buf[0] = Header.enet_dstaddr.addr[0];
+		buf[1] = Header.enet_dstaddr.addr[1];
+		buf[2] = Header.enet_dstaddr.addr[2];
+		buf[3] = Header.enet_dstaddr.addr[3];
+		buf[4] = Header.enet_dstaddr.addr[4];
+		buf[5] = Header.enet_dstaddr.addr[5];
+
+		buf[6] = Header.enet_srcaddr.addr[0];
+		buf[7] = Header.enet_srcaddr.addr[1];
+		buf[8] = Header.enet_srcaddr.addr[2];
+		buf[9] = Header.enet_srcaddr.addr[3];
+		buf[10] = Header.enet_srcaddr.addr[4];
+		buf[11] = Header.enet_srcaddr.addr[5];
+
+		buf[12] = Header.enet_type[0];
+		buf[13] = Header.enet_type[1];
+
+		for (int i = 0; i < length; i++)
+			buf[14 + i] = input[i];
+
+		return buf;
+	}
 
 	public boolean Send(byte[] input, int length) {
-	
-
+		byte[] type = {(byte)0x00, (byte) 0x01}; //type 설정
+		for (int i = 0; i < 2; i++) m_sHeader.enet_type[i] = type[i];
+		byte[] bytes = ObjToByte(m_sHeader, input, length);
+		this.GetUnderLayer().send(bytes, length + 14);
 		return false;
 	}
 
 	
 
+	public byte[] RemoveEtherHeader(byte[] input, int length) {
+		byte[] data = new byte[length - 14];
+		for (int i = 0; i < length - 14; i++)
+			data[i] = input[14 + i];
+		return data;
+	}
+
 	public boolean Receive(byte[] input) {
+		if(IsItMine(input) && input[12] == (byte) 0x08 || IsItMyPacket(input) && IsItBroadcast(input)){//내주소이고 broadcast이거나,  나일시 
+				byte[] datas = RemoveEtherHeader(input, input.length);
+				if(input[13] == (byte) 0x00) // IP 0x08 [00]
+					 this.GetUpperLayer(1).Receive(datas);
+				else if(input[13] == (byte) 0x06) // ARP 0x08 [06]
+					this.GetUpperLayer(0).receive(datas);
+				else return false;
+		}else{
+			return false;
+		}
 		
 		return true;
 	}
 
+	
 	@Override
 	public void SetUnderLayer(BaseLayer pUnderLayer) {
-	
+		// TODO Auto-generated method stub
+		if (pUnderLayer == null)
+			return;
+		this.p_UnderLayer = pUnderLayer;
 	}
 
 	@Override
 	public void SetUpperLayer(BaseLayer pUpperLayer) {
-		
+		// TODO Auto-generated method stub
+		if (pUpperLayer == null)
+			return;
+		this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
+		// nUpperLayerCount++;
 	}
 
 	@Override
@@ -81,17 +132,55 @@ public class EthernetLayer implements BaseLayer {
 
 	@Override
 	public BaseLayer GetUnderLayer() {
-		return  null;
+		// TODO Auto-generated method stub
+		if (p_UnderLayer == null)
+			return null;
+		return p_UnderLayer;
 	}
 
 	@Override
 	public BaseLayer GetUpperLayer(int nindex) {
-		return null;
+		// TODO Auto-generated method stub
+		if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+			return null;
+		return p_aUpperLayer.get(nindex);
 	}
 
 	@Override
 	public void SetUpperUnderLayer(BaseLayer pUULayer) {
-		
+		this.SetUpperLayer(pUULayer);
+		pUULayer.SetUnderLayer(this);
 
+	}
+
+	public boolean IsItMyPacket(byte[] input) {
+		for (int i = 0; i < 6; i++) {
+			if (m_sHeader.enet_srcaddr.addr[i] == input[6 + i])
+				continue;
+			else
+				return false;
+		}
+		return true;
+	}
+
+	public boolean IsItMine(byte[] input) {
+		for (int i = 0; i < 6; i++) {
+			if (m_sHeader.enet_srcaddr.addr[i] == input[i])
+				continue;
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean IsItBroadcast(byte[] input) {
+		for (int i = 0; i < 6; i++) {
+			if (input[i] == -1) {
+				continue;
+			} else
+				return false;
+		}
+		return true;
 	}
 }
